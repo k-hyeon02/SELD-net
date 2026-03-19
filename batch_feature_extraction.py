6669874372d870a28a20ab0530f5887a4a629c2d
Kyu-Hyeon Lee <nbalee00@khu.ac.kr>
2026-03-19 14:00:40 +0900

    주석 수정 및 추가

--- batch_feature_extraction.py
+++ batch_feature_extraction.py
@@ -6,14 +6,14 @@ import cls_feature_class
 dataset_name = 'ansim'  # Datasets: ansim, resim, cansim, cresim, real, mansim and mreal
 
 # Extracts feature and labels for all overlap and splits
-for ovo in [1, 2, 3]:  # SE overlap. Change to [1] if you are only calculating the features for overlap 1.
+for ovo in [1, 2, 3]:  # Sound Event overlap : overlap(동시에 겹치는 음원 수) 1,2,3 순회, Change to [1] if you are only calculating the features for overlap 1.
     for splito in [1, 2, 3]:    # all splits. Use [1, 8, 9] for 'real' and 'mreal' datasets. Change to [1] if you are only calculating features for split 1.
-        for nffto in [512]: # For now use 512 point FFT. Once you get the code running, you can play around with this.
+        for nffto in [512]:  # For now use 512 point FFT. Once you get the code running, you can play around with this.
             feat_cls = cls_feature_class.FeatureClass(ov=ovo, split=splito, nfft=nffto, dataset=dataset_name)
 
             # Extract features and normalize them
-            feat_cls.extract_all_feature()
-            feat_cls.preprocess_features()
+            feat_cls.extract_all_feature()  # wav → 스펙트로그램 → .npy 저장
+            feat_cls.preprocess_features()  # train으로 scaler fit → 전체 파일 정규화 → .npy 저장
 
             # # Extract labels in regression mode
-            feat_cls.extract_all_labels('regr', 0)
+            feat_cls.extract_all_labels('regr', 0)  # csv → SE/DOA 레이블 배열 → .npy 저장
